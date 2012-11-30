d3c51a092955151609e239a0e140b55263b3e310
Dillon Hicks <chronodynamic@gmail.com>
2012-11-30 11:36:14 -0800

    adding order of signals
    In accordance with BB rule 1.3

---
+++ parsing/citations/signals/order.py
@@ -0,0 +1,33 @@
+# Structure and Use of Legal Citations: 
+#   Order of Signals
+#
+# Source: 
+#  The Bluebook 18th Ed. (2008). p48 (R1)
+#
+# 
+
+import .definitions
+
+########################################
+# (rule 1.3)
+# Order of Signals
+#
+# The BB states that the order of signals should be in accordance to
+# their order in rule 1.2 and that the cited authorites must conform
+# to rule 1.4.
+########################################
+ 
+ordered_signals = [
+    NO_SIGNAL,
+    EG,
+    ACCORD,
+    SEE,
+    SEE_ALSO,
+    CF,
+    COMPARE,
+    WITH,
+    CONTRA,
+    BUT_SEE,
+    BUT_CF,
+    SEE_GENERALLY
+]
