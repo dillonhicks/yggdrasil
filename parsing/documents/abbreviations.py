8362d8d863f817325ef39ba58982fa49bb38e403
hhicks <hhicks@ubuntu.(none)>
2012-11-30 10:24:17 -0800

    Abbrev's for court docs
    Found in the blue book. In the docstring for the module I tried to summarize what I read in the blue book about what should be done for abbrevations.

---
+++ parsing/documents/abbreviations.py
@@ -0,0 +1,99 @@
+# Abbreviations for Court Documents
+#
+# Source: 
+#  The Bluebook 18th Ed. (2008). p25-26 / BT-1
+#
+# Rules and Suggestions:
+# 
+# - The following are the *suggested* abbreviations (or lack of
+#   abbreviation, if appropriate) for common words in legal documents.
+#
+# - Further, the BB states that words of 6+ characters may be
+#   abbreviated if the abbreviation is unambiguous to the reader.
+#
+#      - For such titles, it is recommended to omit articles and
+#        prepositions if it does not introduce ambiguity. 
+# 
+#      - For any given document, if an ad-hoc abbreviation is used it
+#        should be consistent throughout the document.
+#
+
+# Possible Todos:
+# 
+# - Create dictionaries mapping abr->word and word->abr
+# - Create regexes to match these in documents.
+# - Find a way to give these "context"
+
+# Abbrevation Definitions
+#
+ADMISSION = "Admis."
+AFFIDAVIT = "Aff."
+AFFIRM = "Affirm"
+AMENDED = "Am."
+ANSWER = "Answer"
+APPEAL = "Appeal"
+APPELLANT = "Appellant"
+APPELLEE = "Appellee"
+APPENDIX = "App."
+ATTACHMENT = "Attach."
+ATTORNEY = "Att'y"
+ATTORNEYS = "Att'ys"
+BRIEF = "Br."
+CERTIORARI = "Cert."
+COMPEL = "Compel"
+COMPLAINT = "Compl."
+COUNTERCLAIM = "Countercl."
+COURT = "Ct."
+CROSS_CLAIM = "Cross-cl."
+DECLARATION = "Decl."
+DEFENDANT = "Def."
+DEFENDANTS = "Def.'s"
+DEFENDANTS2 = "Defs.s"
+DEMURRER = "Dem."
+DENY = "Den."
+DENYING = "Den."
+DEPOSITION = "Dep."
+DISCOVERY = "Disc."
+DISMISS = "Dismiss"
+DOCUMENT = "Doc."
+DOCUMENTS = "Docs."
+EXHIBIT = "Ex."
+GRANT = "Grant"
+HEARING = "Hr'g"
+INJUNCTION = "Inj."
+INTERROGATORY = "Interrog."
+INTERROGATORIES = "Interrogs."
+JOINT_APPENDIX = "J.A."
+JUDGMENT = "J."
+MEMORANDUM = "Mem."
+MINUTES = "Mins."
+MOTION = "Mot."
+OPINION = "Op."
+OPPOSITION = "Opp'n"
+ORDER = "Order"
+PETITION = "Pet."
+PETITIONER = "Pet'r"
+PETITIONERS = "Pet'r's"
+PETITIONERS2 = "Pet'rs'"
+POINTS_AND_AUTHORITIES = "P. & A."
+PRELIMINARY = "Prelim."
+PRODUCE = "Produc."
+PRODUCTION = "Produc."
+QUASH = "Quash"
+RECONSIDERATION = "Recons."
+RECORD = "R."
+REHEARING = "Reh'g"
+REPLY = "Reply"
+REPORTER = "Rep."
+REQUEST = "Req."
+RESPONDENT = "Resp't"
+RESPONSE = "Resp."
+STAY = "Stay"
+SUBPOENA = "Subpoena"
+SUMMARY = "Summ."
+SUPPORT = "Supp."
+SUPPRESS = "Suppress"
+TEMPORARY_RESTRAINING_ORDER = "TRO"
+TESTIMONY = "Test."
+TRANSCRIPT = "Tr."
+VERIFIED_STATEMENT = "V.S."
